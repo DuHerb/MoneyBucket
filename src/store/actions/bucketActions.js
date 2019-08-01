1acb9d26b711eeb27db21cfbae9814adccd53bfd
Dustin Herboldshimer <dustnpdx@gmail.com>
2019-08-01 04:49:27 -0700

    clean up code

--- src/store/actions/bucketActions.js
+++ src/store/actions/bucketActions.js
@@ -79,7 +79,6 @@ export const toggleIsLocked = (bucket) => {
 }
 
 export const reorderArray = (list, startIndex, endIndex) => {
-
   const result = Array.from(list);
   const [removed] = result.splice(startIndex, 1);
   result.splice(endIndex, 0, removed)
