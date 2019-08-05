cff353a473587404e84a880417329bc3c4afc572
Dustin Herboldshimer <dustnpdx@gmail.com>
2019-08-04 20:08:07 -0700

    Updates

---
+++ precache-manifest.28d60b8f3d4c009396f032a1bea8047f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70ed6a12354052456cd0041a2fe7f70d",
+    "url": "/MoneyBucket/index.html"
+  },
+  {
+    "revision": "9dbe0fc659f89282eb60",
+    "url": "/MoneyBucket/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "0f06ab1e4bd49ac99dfd",
+    "url": "/MoneyBucket/static/js/2.2ab0fa2b.chunk.js"
+  },
+  {
+    "revision": "9dbe0fc659f89282eb60",
+    "url": "/MoneyBucket/static/js/main.41d3289d.chunk.js"
+  },
+  {
+    "revision": "b34e8b593c8533c18de4",
+    "url": "/MoneyBucket/static/js/runtime~main.f1332632.js"
+  }
+]);
